062ad5be367c039fb384ce914cd7fdeb8d35584c
Sébastien Rombauts <sebastien.rombauts@cofelyineo-gdfsuez.com>
2016-09-02 17:36:01 +0200

    Add a simplified main.cpp from SQLiteCpp "example1", an appropriate CMake config file and build.sh/.bat scripts

---
+++ build.bat
@@ -0,0 +1,21 @@
+@REM Copyright (c) 2012-2016 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+@REM
+@REM Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+@REM or copy at http://opensource.org/licenses/MIT)
+mkdir build
+cd build
+
+@REM Generate a Visual Studio solution for latest version found
+cmake ..
+if ERRORLEVEL 1 goto onError
+
+@REM Build default configuration (ie 'Debug')
+cmake --build .
+if ERRORLEVEL 1 goto onError
+
+cd ..
+exit
+
+:onError
+@echo An error occured!
+cd ..
