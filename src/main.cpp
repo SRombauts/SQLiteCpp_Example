988d1abe42f02fb8063662ac27afbb52ee92dea9
Tien Do <tiendq@gmail.com>
2018-11-01 22:35:26 +0700

    refactor: remove pthread linking, fix misc. code conventions

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@
  * or copy at http://opensource.org/licenses/MIT)
  */
 
+#include <exception>
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
 
 #include <SQLiteCpp/SQLiteCpp.h>
 
+using namespace std;
 
 #ifdef SQLITECPP_ENABLE_ASSERT_HANDLER
 namespace SQLite
@@ -30,55 +32,56 @@ void assertion_failed(const char* apFile, const long apLine, const char* apFunc,
 }
 #endif
 
-int main ()
+int main()
 {
     // Using SQLITE_VERSION would require #include <sqlite3.h> which we want to avoid: use SQLite::VERSION if possible.
-//  std::cout << "SQlite3 version " << SQLITE_VERSION << std::endl;
-    std::cout << "SQlite3 version " << SQLite::VERSION << " (" << SQLite::getLibVersion() << ")" << std::endl;
-    std::cout << "SQliteC++ version " << SQLITECPP_VERSION << std::endl;
+    // cout << "SQlite3 version " << SQLITE_VERSION << '\n';
+    cout << "SQlite3 version " << SQLite::VERSION << " (" << SQLite::getLibVersion() << ")" << '\n';
+    cout << "SQliteC++ version " << SQLITECPP_VERSION << '\n';
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Simple batch queries example :
+    // Simple batch queries example
     try
     {
         // Open a database file in create/write mode
-        SQLite::Database    db("test.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
-        std::cout << "SQLite database file '" << db.getFilename().c_str() << "' opened successfully\n";
+        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+        cout << "SQLite database file '" << db.getFilename() << "' opened successfully\n";
 
         // Create a new table with an explicit "id" column aliasing the underlying rowid
         db.exec("DROP TABLE IF EXISTS test");
         db.exec("CREATE TABLE test (id INTEGER PRIMARY KEY, value TEXT)");
 
         // first row
-        int nb = db.exec("INSERT INTO test VALUES (NULL, \"test\")");
-        std::cout << "INSERT INTO test VALUES (NULL, \"test\")\", returned " << nb << std::endl;
+        int nb = db.exec("INSERT INTO test VALUES (NULL, \"first\")");
+        cout << "INSERT INTO test VALUES (NULL, \"first\")\", returned " << nb << '\n';
 
         // second row
         nb = db.exec("INSERT INTO test VALUES (NULL, \"second\")");
-        std::cout << "INSERT INTO test VALUES (NULL, \"second\")\", returned " << nb << std::endl;
+        cout << "INSERT INTO test VALUES (NULL, \"second\")\", returned " << nb << '\n';
 
         // update the second row
         nb = db.exec("UPDATE test SET value=\"second-updated\" WHERE id='2'");
-        std::cout << "UPDATE test SET value=\"second-updated\" WHERE id='2', returned " << nb << std::endl;
+        cout << "UPDATE test SET value=\"second-updated\" WHERE id='2', returned " << nb << '\n';
 
-        // Check the results : expect two row of result
-        SQLite::Statement   query(db, "SELECT * FROM test");
-        std::cout << "SELECT * FROM test :\n";
+        // Check the results: expect two rows
+        SQLite::Statement query(db, "SELECT * FROM test");
+        cout << "SELECT * FROM test\n";
+
         while (query.executeStep())
         {
-            std::cout << "row (" << query.getColumn(0) << ", \"" << query.getColumn(1) << "\")\n";
+            cout << "row (" << query.getColumn(0) << ", \"" << query.getColumn(1) << "\")\n";
         }
 
         db.exec("DROP TABLE test");
     }
-    catch (std::exception& e)
+    catch (exception const &e)
     {
-        std::cout << "SQLite exception: " << e.what() << std::endl;
-        return EXIT_FAILURE; // unexpected error : exit the example program
+        cout << "SQLite exception: " << e.what() << '\n';
+        return EXIT_FAILURE;
     }
+
     remove("test.db3");
 
-    std::cout << "everything ok, quitting\n";
+    cout << "Everything OK, quitting\n";
 
     return EXIT_SUCCESS;
 }
